3817999524e5ff3639a90e31fd2e1ed1e4482aad
mbunix <bellianotieno24@gmail.com>
2023-05-19 09:27:45 +0300

    changes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,13 +8,14 @@ function navbar() {
       <div className='navbar-logo'>
         <img className='navbar-logo-img' src={Logo} alt='logo picture' />
           <h2>Sarova Holdings</h2>
+      </div>
           <div className='navbar-buttons'>
             <button className='btn-home' >Home</button>
             <button className='btn-about' >About</button>
             <button className='btn-contact-us' >Contact - Us</button>
             <button className='btn- highlights' >Highlights</button>
           </div>
-      </div>
+      
       </div>
       <div className=' navbar-light'>
         <h2>Welcome to Sarova Holding  Investment Company</h2>
